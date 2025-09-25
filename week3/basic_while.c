1d38556de8dfa3d497f9a814b79008311ac74e58
yoyo899843 <153046170+yoyo899843@users.noreply.github.com>
2025-09-25 14:58:50 +0000

    0925

---
+++ week3/basic_while.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void){
+    int Product = 3;
+    while (Product<=100)
+    {
+        Product = Product*3;
+    }
+    printf("Product=%d", Product);
+}
--- week3/single_selection.c
+++ week3/single_selection.c
@@ -1,7 +1,24 @@
 #include<stdio.h>
 
-
 int main(void){
-
-
+    int Score;
+    scanf("%d", &Score);
+    if(Score>=90){
+        printf("Grade A");
+    }
+    else if (Score>=80)
+    {
+        printf("Grade B");
+    }
+    else if (Score>=70)
+    {
+        printf("Grade C");
+    }
+    else
+    {
+        printf("Grade D");
+    }
+    
+    
+    
 }
