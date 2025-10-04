8552ddf584c08ff59862cc49f54891ac2967e98f
yoyo899843 <153046170+yoyo899843@users.noreply.github.com>
2025-10-04 15:12:52 +0000

    1004

---
+++ week5/114403519_A2.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+
+int UserNum_Check(UserNum){
+    while(1){
+        if (length(UserNum) != 4){
+            printf("Invalid input! Please enter a 4-digit number.\n");
+            printf("Enter a 4-digit number to generate a password:\n");
+            scanf("%d", UserNum);
+        }
+        else{
+            return UserNum;
+        }
+    }
+}
+
+int Password_Generater(UserNum){
+    Password = ((UserNum * 3) + 1357) % 10000
+}
+
+int main(void){
+    int UserNum;
+    printf("Enter a 4-digit number to generate a password:\n");
+    scanf("%d", UserNum);
+    UserNum = UserNum_Check(UserNum);
+
+}
