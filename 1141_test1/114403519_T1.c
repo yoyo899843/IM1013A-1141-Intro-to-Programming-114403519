1f705a0c6f09fe51b0594f47609b72e4c5995c1b
yoyo899843 <153046170+yoyo899843@users.noreply.github.com>
2025-10-17 03:05:32 +0000

    1017

--- 1141_test1/114403519_T1.c
+++ 1141_test1/114403519_T1.c
@@ -4,21 +4,21 @@ void Option_1(){
     int Adult, Student;
     while(1){
         while(1){
-            printf("%s", "[Option 1]Please enter how many adult ticket(s) you want to buy? (1-20, or -1 to end the program)\n");
+            printf("%s", "\n[Option 1]Please enter how many adult ticket(s) you want to buy? (1-20, or -1 to end the program)\n");
             scanf("%d", &Adult);    
-            if(Adult == -1) break;
+            if(Adult == -1) return 0;
             if(Adult>=1 && Adult<=20){
                 break;
             }
             printf("%s", "The number should be a integer between 1 and 20\n");
         }
-        if(Adult == -1) break;
+        if(Adult == -1) return 0;
         while(1){
             printf("%s", "[Option 1]Please enter how many student ticket(s) you want to buy? (1-20, or -1 to end the program)\n");
             scanf("%d", &Student);
             if(Student == -1) break;
             if(Student>=1 && Student<=20){
-                break;
+                break;;
             }
             printf("%s", "The number should be a integer between 1 and 20\n");
         }
@@ -45,7 +45,7 @@ void Option_1(){
         Total_Amount = ((Adult*200.0)+(Student*150.0))*Discount;
         if(Bonus == 1) Total_Amount = Total_Amount*0.95;
 
-        printf("---Quotation Summary---\n");
+        printf("\n---Quotation Summary---\n");
         printf("Adult: %d, Student: %d, Total Ticket: %d\n", Adult, Student, Adult+Student);
         printf("Base Total: %.2f\n", Adult*200.0+Student*150.0);
         printf("Quantity Discount: %.2f\n", Discount);
@@ -58,12 +58,12 @@ void Option_1(){
 int main(){
     while(1){
         int Option;
-        printf("%s", "===== Future City Expo Ticket System =====\n");
+        printf("%s", "\n===== Future City Expo Ticket System =====\n");
         printf("%s", "1) Option 1 - Single-Order Quotation\n2) Option 2 - Group Total & Installments\n-1) End the Program\n");
         scanf("%d", &Option);
-        if(Option == 1);
+        if(Option == 1) Option_1();
         else if(Option == 2);
-        else if(Option == -1) return 0;        
-    }
-    
+        else if(Option == -1) return 0;
+        else printf("%s", "Please enter 1, 2 or -1\n");
+    }   
 }
