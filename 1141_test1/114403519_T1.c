36080613a3b35fc8a097420758769d74a57a72d6
yoyo899843 <153046170+yoyo899843@users.noreply.github.com>
2025-10-17 03:42:02 +0000

    1017

--- 1141_test1/114403519_T1.c
+++ 1141_test1/114403519_T1.c
@@ -1,4 +1,5 @@
 # include <stdio.h>
+# include<math.h>
 
 void Option_1(){
     int Adult, Student;
@@ -55,15 +56,70 @@ void Option_1(){
     }
 }
 
+void Option_2(){
+    float Principal, Handling_Fee, Interest_Rate, Total_Repayment;
+    int Period;
+
+    while(1){
+        while(1){
+            printf("%s", "[Option 2] Enter principal in dollars (positive), or -1 to end:\n");
+            scanf("%f", &Principal);
+            if(Principal>0) break;
+            printf("%s", "Please enter positive number\n");
+        }
+        
+        if(Principal<501 && Principal>0){
+            Period = 1;
+            Handling_Fee = 0.0;
+            Interest_Rate = 0.0;
+        }
+        else if(Principal<1001 && Principal>500){
+            Period = 3;
+            Handling_Fee = 20.0;
+            Interest_Rate = 0.03;
+        }
+        else if(Principal<2001 && Principal>1000){
+            Period = 6;
+            Handling_Fee = 17.0;
+            Interest_Rate = 0.025;
+        }
+        else if(Principal>2000){
+            Period = 12;
+            Handling_Fee = 12;
+            Interest_Rate = 0.015;
+        }
+        else if(Principal == -1) return 0;
+
+        Total_Repayment = Principal*pow(1+Interest_Rate,Period)+(Handling_Fee*Period);
+
+        printf("%s", "\n----- Installment Plan Summary-----\n");
+        printf("Principal: %.2f\n", Principal);
+        printf("Number of Periods: %d\n", Period);
+        printf("Handling Fee per Period: %.2f\n", Handling_Fee);
+        printf("Interest Rate per Period: %.2f%% \n", Interest_Rate*100);
+        printf("Repayment (Compound in Principle): %.2f\n", Principal*pow(1+Interest_Rate,Period));
+        printf("Total Handling Fee: %.2f\n", Handling_Fee*Period);
+        printf("Total Repayment (Including Handling Fee): %.2f\n", Total_Repayment);
+        printf("Payment per Installment: %.2f\n\n", Total_Repayment/Period);
+    }
+}
+
 int main(){
     while(1){
         int Option;
         printf("%s", "\n===== Future City Expo Ticket System =====\n");
         printf("%s", "1) Option 1 - Single-Order Quotation\n2) Option 2 - Group Total & Installments\n-1) End the Program\n");
         scanf("%d", &Option);
-        if(Option == 1) Option_1();
-        else if(Option == 2);
+        if(Option == 1){
+            Option_1();
+            break;
+        }
+        else if(Option == 2){
+            Option_2();
+            break;
+        }
         else if(Option == -1) return 0;
         else printf("%s", "Please enter 1, 2 or -1\n");
-    }   
+    }
+    return 0;
 }
--- 1141_test1/2.c
+++ 1141_test1/2.c
@@ -1,39 +1,50 @@
 # include<stdio.h>
+# include<math.h>
 
 int main(){
-    float Principal, Handling_Fee, Interest_Rate;
+    float Principal, Handling_Fee, Interest_Rate, Total_Repayment;
     int Period;
-    printf("%s", "[Option 2] Enter principal in dollars (positive), or -1 to end:\n");
-    scanf("%d", &Principal);
-    
-    if(Principal<501 && Principal>0){
-        Period = 1;
-        Handling_Fee = 0.0;
-        Interest_Rate = 0.0;
+
+    while(1){
+        while(1){
+            printf("%s", "[Option 2] Enter principal in dollars (positive), or -1 to end:\n");
+            scanf("%f", &Principal);
+            if(Principal>0) break;
+            printf("%s", "Please enter positive number\n");
+        }
+        
+        if(Principal<501 && Principal>0){
+            Period = 1;
+            Handling_Fee = 0.0;
+            Interest_Rate = 0.0;
+        }
+        else if(Principal<1001 && Principal>500){
+            Period = 3;
+            Handling_Fee = 20.0;
+            Interest_Rate = 0.03;
+        }
+        else if(Principal<2001 && Principal>1000){
+            Period = 6;
+            Handling_Fee = 17.0;
+            Interest_Rate = 0.025;
+        }
+        else if(Principal>2000){
+            Period = 12;
+            Handling_Fee = 12;
+            Interest_Rate = 0.015;
+        }
+        else if(Principal == -1) return 0;
+
+        Total_Repayment = Principal*pow(1+Interest_Rate,Period)+(Handling_Fee*Period);
+
+        printf("%s", "\n----- Installment Plan Summary-----\n");
+        printf("Principal: %.2f\n", Principal);
+        printf("Number of Periods: %d\n", Period);
+        printf("Handling Fee per Period: %.2f\n", Handling_Fee);
+        printf("Interest Rate per Period: %.2f%% \n", Interest_Rate*100);
+        printf("Repayment (Compound in Principle): %.2f\n", Principal*pow(1+Interest_Rate,Period));
+        printf("Total Handling Fee: %.2f\n", Handling_Fee*Period);
+        printf("Total Repayment (Including Handling Fee): %.2f\n", Total_Repayment);
+        printf("Payment per Installment: %.2f\n\n", Total_Repayment/Period);
     }
-    else if(Principal<1001 && Principal>500){
-        Period = 3;
-        Handling_Fee = 20.0;
-        Interest_Rate = 0.03;
-    }
-    else if(Principal<2001 && Principal>1000){
-        Period = 6;
-        Handling_Fee = 17.0;
-        Interest_Rate = 0.025;
-    }
-    else if(Principal>2000){
-        Period = 12;
-        Handling_Fee = 12;
-        Interest_Rate = 0.015;
-    }
-    else if(Principal == -1) return 0;
-    printf("%s", "\n----- Installment Plan Summary-----\n");
-    printf("Principal: %.2f\n", Principal);
-    printf("Number of Periods: %d\n", Period);
-    printf("Handling Fee per Period: %.2f\n", Handling_Fee);
-    printf("Interest Rate per Period: %.2f\n", Interest_Rate);
-    printf("Repayment: %f", );
-    printf("Total Handling Fee: %f", );
-    printf("Total Repayment (Including Handling Fee): %d", );
-    printf("Payment per Installment: %d", );
 }
