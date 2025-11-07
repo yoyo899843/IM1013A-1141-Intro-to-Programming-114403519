9d383fc5fc626313d8ce22d8b7c07ed367628209
施佑佑 <153046170+yoyo899843@users.noreply.github.com>
2025-11-07 02:25:30 +0000

    1107

--- week7/nested_exercise1.c
+++ week7/nested_exercise1.c
@@ -8,14 +8,15 @@ int main(void){
 
     printf("%s", "Enter the column you want\n");
     scanf("%d", &col);
+    printf("%s", "=====================================\n");
 
     counter = 1;
     for(int i=row; i>=1; i--){
-        for(int j=col; j>=2; j--){
-            printf("%-2d ", counter);
+        for(int j=col; j>=1; j--){
+            printf("%3d ", counter);
             counter++;
         }
-        printf("%-2d\n", counter);
+        printf("%3d\n", counter);
         counter++;
     }
 }
