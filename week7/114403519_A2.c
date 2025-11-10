fb349f1293a1cb3022b9387b4b77ae9f75604014
yoyo899843 <yoyo899843@gmail.com>
2025-11-10 15:43:01 +0800

    1110

--- week7/114403519_A2.c
+++ week7/114403519_A2.c
@@ -1,6 +1,6 @@
 # include <stdio.h>
 
-int multiplication(){
+int multiplication(int a, int b){
     for(int i=1; i<10; i++){
         for(int j=1; j<10; j++){
             printf("%d*%d=%2d  ", i, j, i*j);
@@ -9,7 +9,9 @@ int multiplication(){
     }
 }
 
-int 
+int reverse_number(int n){
+    
+}
 
 int main(void){
     int option;
