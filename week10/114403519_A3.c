af6077df4cbbe4cd9d9e993a5f4120171931a315
yoyo899843 <yoyo899843@gmail.com>
2025-12-05 15:38:31 +0800

    1205

--- week10/114403519_A3.c
+++ week10/114403519_A3.c
@@ -3,18 +3,19 @@
 
 int main(void){
     int option;
-    printf("%s", "===== MENU =====\n1) Option 1 - Split into even and odd number array\n2) Option 2 - Calculate Sum of Elements in the Upper Triangle\n(-1) End program\nPlease select an option: ");
-    scanf("%d", &option);
-    switch (option)
-    {
-    case 1:
-        break;
-    case 2:
-        break;
-    case (-1):
-        printf("%s", "Program terminated.\n");
-    default:
-        printf("%s", "Invalid option. Try again.\n");
+    while(1){
+        printf("%s", "===== MENU =====\n1) Option 1 - Split into even and odd number array\n2) Option 2 - Calculate Sum of Elements in the Upper Triangle\n(-1) End program\nPlease select an option: ");
+        scanf("%d", &option);
+        switch (option)        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case (-1):
+            printf("%s", "\nProgram terminated.\n");
+            return 0;
+        default:
+            printf("%s", "\nInvalid option. Try again.\n\n");
+        }
     }
-
 }
