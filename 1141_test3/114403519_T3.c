1d5a78827026705f7696ddfde34be78eb94b3977
施佑佑 <153046170+yoyo899843@users.noreply.github.com>
2025-12-19 03:51:58 +0000

    1219

--- 1141_test3/114403519_T3.c
+++ 1141_test3/114403519_T3.c
@@ -56,6 +56,32 @@ void sort_ascending(int n, int B[n]){
     printf("\n\n");
 }
 
+void find_max_resource_block(int N, int map[N][N]){
+    int sum=0, tmp=0, top=0, left=0;
+    for(int i=0; i<=N-3; i++){
+        for(int j=0; j<=N-3; j++){
+            sum = 0;
+            for(int k=i; k<i+3; k++){
+                for(int l=j; l<j+3; l++){
+                    sum = sum + map[k][l];
+                }
+            }
+            if(sum>tmp){
+                tmp = sum;
+                top = i;
+                left = j;
+            }
+        }
+    }
+    printf("\nMaximum resource sum: %d\nTop-left coordinate: (%d, %d)\nMax resource block:\n", tmp, top, left);
+    for(int i=top; i<top+3; i++){
+        for(int j=left; j<left+3; j++){
+            printf("%d ", map[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 
 int main(void){
     int option;
@@ -113,7 +139,24 @@ int main(void){
             break;
         }
         case 3:
+        {
+            printf("\nPlease enter the size of the matix(3~10):");
+            int N;
+            scanf("%d", &N);
+            int n = N*N;
+            int map[N][N];
+            printf("Matrix M ( %d x %d )\n",N ,N);
+            srand(time(NULL));
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++){
+                    map[i][j]=rand()%10 ;
+                    printf("%d ", map[i][j]);
+                }
+                printf("\n");
+            }
+            find_max_resource_block(N, map);
             break;
+        }
         case -1:
             printf("Program ends.\n");
             return 0;
--- 1141_test3/3.c
+++ 1141_test3/3.c
@@ -2,8 +2,30 @@
 # include<stdlib.h>
 # include<time.h>
 
-void find_max_resource_block(int N, int map[][N]){
-
+void find_max_resource_block(int N, int map[N][N]){
+    int sum=0, tmp=0, top=0, left=0;
+    for(int i=0; i<=N-3; i++){
+        for(int j=0; j<=N-3; j++){
+            sum = 0;
+            for(int k=i; k<i+3; k++){
+                for(int l=j; l<j+3; l++){
+                    sum = sum + map[k][l];
+                }
+            }
+            if(sum>tmp){
+                tmp = sum;
+                top = i;
+                left = j;
+            }
+        }
+    }
+    printf("\nMaximum resource sum: %d\nTop-left coordinate: (%d, %d)\nMax resource block:\n", tmp, top, left);
+    for(int i=top; i<top+3; i++){
+        for(int j=left; j<left+3; j++){
+            printf("%d ", map[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main(void){
@@ -11,14 +33,15 @@ int main(void){
     int N;
     scanf("%d", &N);
     int n = N*N;
-    int M[N][N], B[n];
-    printf("[Matrix M ( %d x %d )]\n",N ,N);
+    int map[N][N];
+    printf("Matrix M ( %d x %d )\n",N ,N);
     srand(time(NULL));
     for(int i=0; i<N; i++){
         for(int j=0; j<N; j++){
-            M[i][j]=rand()%10 ;
-            printf("%d ", M[i][j]);
+            map[i][j]=rand()%10 ;
+            printf("%d ", map[i][j]);
         }
         printf("\n");
     }
+    find_max_resource_block(N, map);
 }
